6ec6ca2fa3b6b2e7500c6d6e1454637527c1465d
Jong-Dae Park <jdpark7@gmail.com>
2025-05-07 14:08:26 +0900

    Update elegant_ota_update.ino

--- week7/elegant_ota_update.ino
+++ week7/elegant_ota_update.ino
@@ -1,1 +1,95 @@
-to be uploaded later
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>  // WiFiClientSecure 사용
+#include <ESP8266HTTPClient.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ElegantOTA.h>
+#include <DHT.h>
+
+#define DHTPIN 0
+#define DHTTYPE DHT11
+#define STUDENTID "123456"
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid = "U+Net3FD3";
+const char* password = "5D833323M@";
+const char* dataserver = "https://iot.learnina.org/dht.php";
+const char* stationid = STUDENTID;
+
+const uint8_t fingerprint[] = {0x5D,0x07,0x6E,0xB5,0x48,0xE8,0x81,0x1F,0x2A,0x42,0x2C,0x14,0x65,0x20,0x23,0xD3,0x1F,0x6C,0xAC,0x85};
+
+AsyncWebServer server(80);
+// WiFiClientSecure client; 
+std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  client->setFingerprint(fingerprint);
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  dht.begin();
+
+  // ★ 서버 인증 무시 설정 (테스트용)
+  // client.setInsecure();
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP8266.");
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  ElegantOTA.begin(&server,"admin","iot601");    // Start ElegantOTA
+}
+
+void loop(void) {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    delay(2000);
+    return;
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient https;  // HTTPClient 그대로 사용합니다. (client가 secure면 https 자동 처리)
+    String url = String(dataserver) + "?stationid=" + stationid + 
+                 "&temperature=" + String(temperature, 1) + 
+                 "&humidity=" + String(humidity, 1);
+
+    Serial.print("Requesting URL: ");
+    Serial.println(url);
+
+    https.begin(*client, url);  // WiFiClientSecure 사용
+
+    int httpCode = https.GET(); // GET 요청
+    if (httpCode > 0) {
+      String payload = https.getString();
+      Serial.println(httpCode);
+      Serial.println(payload);
+    } else {
+      Serial.print("Error on HTTPS request: ");
+      Serial.println(httpCode);
+    }
+    https.end();
+  }
+  ElegantOTA.loop();
+  delay(10000);
+
+}
+
+
