cf44b053247e1de6def0c5e81290c744f07aea54
Jong-Dae Park <jdpark7@gmail.com>
2025-05-20 23:15:29 +0900

    Update admin.py

--- week10/iot/admin.py
+++ week10/iot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SensorData
+
+@admin.register(SensorData)
+class SensorDataAdmin(admin.ModelAdmin):
+    list_display=('temperature','humidity','timestamp')
+
