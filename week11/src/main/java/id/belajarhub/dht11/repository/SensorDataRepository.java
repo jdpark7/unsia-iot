83b82712c5d3a75f530d17ea4d28b969e02ddb03
Jong-Dae Park <jdpark7@gmail.com>
2025-05-25 06:41:11 +0900

    Update SensorDataRepository.java

--- week11/src/main/java/id/belajarhub/dht11/repository/SensorDataRepository.java
+++ week11/src/main/java/id/belajarhub/dht11/repository/SensorDataRepository.java
@@ -1,9 +1,13 @@
 package id.belajarhub.dht11.repository;
 
-import id.belajarhub.dht11.model.SensorData;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import id.belajarhub.dht11.model.SensorData;
 
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
     List<SensorData> findByDeviceId(String deviceId);
+    List<SensorData> findByDeviceIdOrderByTimestampDesc(String deviceId, Pageable pageable);
 }
