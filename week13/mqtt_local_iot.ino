818407f38b3c3fe5f60bdd40d1f85c15c50edc8c
Jong-Dae Park <jdpark7@gmail.com>
2025-06-11 06:21:09 +0900

    Update mqtt_belajarhub.ino

--- week13/mqtt_local_iot.ino
+++ week13/mqtt_local_iot.ino
@@ -1,31 +1,33 @@
-#include <ESP8266WiFi.h>            
+#include <ESP8266WiFi.h>             
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <DHT.h>
+#include <LittleFS.h>
 
-// Wi-Fi 
+// Wi-Fi setting
 const char* ssid = "your-ssid";
 const char* password = "your-password";
 
-// MQTT 
-const char* mqtt_server = "iot.unsia.local";
-const int mqtt_port = 1883;
-const char* mqtt_user = "unsia";
-const char* mqtt_pass = "unsia*01";
-const char* mqtt_topic = "unsia/iot";
+// MQTT broker settung
+const char* mqtt_server = "mqtt.belajarhub.id";
+const int mqtt_port = 8883;
+const char* mqtt_user = "username"; // change with your username
+const char* mqtt_pass = "username*01"; // change with your username*01
+const char* mqtt_topic = "unsia/unsia/iot"; // Or username/any-topic for you only
 
 // DHT sensor
-#define DHTPIN 0            // pin number for DHT11 sensor data
+#define DHTPIN 0            // pin number
+#define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+#define CA_CERT_FILE "/rootCA_der.h"
 
-#include "rootCA.der.h"  // xxd -i 
+#include "rootCA_der.h"  // xxd -i
 
 WiFiClient plainClient;
 BearSSL::WiFiClientSecure secureClient;
+PubSubClient mqttClient(secureClient);
 
 BearSSL::X509List cert(rootCA_der, rootCA_der_len);  // 
-
-PubSubClient* mqttClient; 
 
 void setup_wifi() {
   delay(100);
@@ -38,41 +40,42 @@ void setup_wifi() {
 }
 
 void reconnect() {
-  while (!mqttClient->connected()) {
+  while (!mqttClient.connected()) {
     Serial.print("Attempting MQTT connection...");
-    if (mqttClient->connect("dhtClient", mqtt_user, mqtt_pass)) {
+    if (mqttClient.connect("dhtClient", mqtt_user, mqtt_pass)) {
       Serial.println("connected");
     } else {
       Serial.print("failed, rc=" );
-      Serial.print(mqttClient->state());
+      Serial.print(mqttClient.state());
       Serial.print("\n" );
       delay(5000);
     }
   }
 }
 
+
 void setup() {
+  char *ca_cert = nullptr;
   Serial.begin(115200);
   dht.begin();
   setup_wifi();
 
-  if (mqtt_port == 8883) {
-    BearSSL::X509List cert(rootCA_der, rootCA_der_len);
-    secureClient.setTrustAnchors(&cert);
-    secureClient.setBufferSizes(512, 512); 
-    mqttClient = new PubSubClient(secureClient);
-  } else {
-    mqttClient = new PubSubClient(plainClient);
-  }
-  mqttClient->setServer(mqtt_server, mqtt_port);
+ 
+  secureClient.setTrustAnchors(&cert);
+  secureClient.setBufferSizes(512, 512); 
+  secureClient.setInsecure();
+  Serial.print("CA cert length: ");
+  Serial.println(rootCA_der_len);
+
+  mqttClient.setServer(mqtt_server, mqtt_port);
 
 }
 
 void loop() {
-  if (!mqttClient->connected()) {
+  if (!mqttClient.connected()) {
     reconnect();
   }
-  mqttClient->loop();
+  mqttClient.loop();
 
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
@@ -80,7 +83,7 @@ void loop() {
   if (!isnan(temp) && !isnan(hum)) {
     char payload[64];
     snprintf(payload, sizeof(payload), "{\"temperature\":%.1f,\"humidity\":%.1f}", temp, hum);
-    mqttClient->publish(mqtt_topic, payload);
+    mqttClient.publish(mqtt_topic, payload);
     Serial.println(payload);
   } else {
     Serial.println("Failed to read from DHT sensor!");
@@ -88,3 +91,4 @@ void loop() {
 
   delay(10000); // 10 sec
 }
+
